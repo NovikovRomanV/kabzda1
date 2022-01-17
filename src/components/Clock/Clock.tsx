f87787381c7505ffbcd6d15a7eff6a95fafbeb09
NovikovRomanV <rimm19845@gmail.com>
2022-01-17 23:06:20 +0300

    up components Clock and add Clock.stories.tsx

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,26 +1,33 @@
 import React, {useEffect, useState} from "react";
 import s from "./Clock.module.css"
+import {clearInterval} from "timers";
+
+const getZeroToTime = (number: number) => number < 10 ? "0" + number : number
 
 export const Clock = () => {
     const clock = () => {
         const time = new Date();
-        const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
-        const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
-        const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-        const timeClock = [time]
+        // const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+        // const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+        // const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+        // const timeClock = [time]
 
         // return timeClock.map(tc => <div>{hours}:{minutes}:{seconds}</div>)
 
         return <div className={s.clock}>
-            {hours} : {minutes} : {seconds}
+            {getZeroToTime(time.getHours())} : {getZeroToTime(time.getMinutes())} : {getZeroToTime(time.getSeconds())}
         </div>
 
     }
     const [count, setCount] = useState(0)
 
     useEffect(() => {
-        setInterval(clock, 1000)
-        setInterval(() => setCount(state => state + 1), 1000)
+        const intervalClockId = setInterval(clock, 1000)
+        const intervalState = setInterval(() => setCount(state => state + 1), 1000)
+        return () => {
+            clearInterval(intervalClockId)
+            clearInterval(intervalState)
+        }
     }, [])
 
     return <>
